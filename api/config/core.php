0ae8905c5e9c01603c98d0e0e04f8518a28c8436
Uche Jude <nuchejude@gmail.com>
2017-11-05 12:41:54 +0100

    Deleted unecessary scripts

--- api/config/core.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // show error reporting
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-    
-    // home page url
-    $home_url="http://localhost/simpleApi-PHP/api/";
- 
-?>
